dfd2a8d2844fadc8ad049a9f9a1ce2f51f212f2d
Gilbert <shikweg@gmail.com>
2023-12-13 19:31:33 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+let divide = function(){
+    return 2000/100
+}    
+console.log(divide())
+let square = "" 
+square = () => {
+    return 2 ** 2
+}
+console.log(square())
+let add = ""
+add = (a,b) => {
+    return a + b
+}
+console.log(add(5,8))
